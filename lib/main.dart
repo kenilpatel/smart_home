bac2d86f40569afcbf20db371c93447a694de5bd
kenilpatel <kenilpatel786@gmail.com>
2019-01-16 00:00:24 +0530

    login+forget+animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 void main()
