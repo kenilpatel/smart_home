65adbb4f43c927606bf8fc64f2f508e95f8b55f1
kenilpatel <kenilpatel786@gmail.com>
2019-01-16 00:04:19 +0530

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 void main()
